77cd16e25e668c4a5033ee72dafaeb423133c192
jaehyeon <violet6520@naver.com>
2021-10-12 10:27:12 +0900

    [2021/10/12 이재현] 백준 최대 힙 문제

---
+++ coding-test-study/src/main/java/baekjoon/자료구조/최대_힙/Main.java
@@ -0,0 +1,33 @@
+package baekjoon.자료구조.최대_힙;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader         br    = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder          sb    = new StringBuilder();
+		PriorityQueue<Integer> queue = new PriorityQueue<Integer>(Collections.reverseOrder());
+		int                    N     = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < N; i++) {
+			int tmp = Integer.parseInt(br.readLine());
+			if (tmp == 0) {
+				if (queue.isEmpty()) {
+					sb.append("0\n");
+				} else {
+					sb.append(queue.poll()+"\n");
+				}
+			} else {
+				queue.add(tmp);
+			}
+
+		}
+		System.out.println(sb.toString());
+	}
+
+}
