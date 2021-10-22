87d0abd5b9bca9ca2ed8c30b9c62fd5ffdec1c85
jaehyuen <violet6520@naver.com>
2021-10-22 20:12:51 +0900

    [2021/10/22 이재현] 백준 팩토리얼 0의 개수 문제

---
+++ coding-test-study/src/main/java/baekjoon/수학/팩토리얼_0의_개수/Main.java
@@ -0,0 +1,21 @@
+package baekjoon.수학.팩토리얼_0의_개수;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+
+		int num = in.nextInt();
+		int count = 0;
+
+		while (num >= 5) {
+			count += num / 5;
+			num /= 5;
+		}
+		System.out.println(count);
+
+	}
+
+}
